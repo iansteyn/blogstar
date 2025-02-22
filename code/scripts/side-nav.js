5c955a1bb6145f90b410e3a5a4b71c66eab32bd6
iansteyn <iansteyn03@gmail.com>
2025-02-21 19:44:11 -0800

    Add code to highlight current page on nav

--- code/scripts/side-nav.js
+++ code/scripts/side-nav.js
@@ -7,6 +7,16 @@ Performs the following dynamic actions:
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    
+    const navLinks = document.querySelectorAll(".nav-link");
 
+    for (navLink of navLinks) {
+        if (navLink.href  === window.location.href
+            && navLink.id != "nav-logo")
+        {
+            navLink.classList.add("current-page");
+        }
+        else if (navLink.classList.contains("current-page")) {
+            navLink.classList.remove("current-page");
+        }
+    }
 });
