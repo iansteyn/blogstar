5bf340d42288d30053bbe34a5a769d02f312f229
iansteyn <iansteyn03@gmail.com>
2025-02-21 20:15:50 -0800

    Remove redundant classlist removal in else statement

--- code/scripts/side-nav.js
+++ code/scripts/side-nav.js
@@ -7,16 +7,29 @@ Performs the following dynamic actions:
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    // HIGHLIGHT NAVLINK OF CURRENT PAGE
+    // ---------------------------------
     const navLinks = document.querySelectorAll(".nav-link");
 
     for (navLink of navLinks) {
-        if (navLink.href  === window.location.href
-            && navLink.id != "nav-logo")
-        {
+        if ((navLink.href  === window.location.href) && (navLink.id != "nav-logo")) {
             navLink.classList.add("current-page");
         }
-        else if (navLink.classList.contains("current-page")) {
-            navLink.classList.remove("current-page");
-        }
     }
+
+    // COLLAPSE/EXPAND NAV
+    // -------------------
+    // const navCollapseButton = document.getElementById("nav-collapse-button");
+
+    // navCollapseButton.addEventListener("onClick", () => {
+    //     console.log('button clicked');
+    //     // get all links
+    //     for (navLink of navLinks) {
+            
+    //     }
+    //     // toggle hide text from links (might have to surround them all in spans for this)
+    //     // toggle class: change collapse logo size
+    //     // toggle class: remove padding extra right padding from icons?
+    //     // swap arrow direction of button content
+    // });
 });
