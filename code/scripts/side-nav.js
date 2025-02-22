967a3129f39f4a9262734e93997f0e47270a0e2a
iansteyn <iansteyn03@gmail.com>
2025-02-21 19:17:00 -0800

    add scripts folder and side-nav.js file

---
+++ code/scripts/side-nav.js
@@ -0,0 +1,12 @@
+/* side-nav.js
+--------------------------------------
+Performs the following dynamic actions:
+- highlighting link of current page
+- collapsing and expanding the navbar on button press
+-------------------------------------- */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    
+
+});
