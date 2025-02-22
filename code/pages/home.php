60943b62037e4b213de638a4860bf862ef6f0860
iansteyn <iansteyn03@gmail.com>
2025-02-21 16:48:32 -0800

    Add home.php

---
+++ code/pages/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+
+  <title>
+    Our Site | Home
+  </title>
+
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../css/side-nav.css">
+</head>
+
+<body>
+  <?php include "../html/side-nav.html" ?>
+  <main>
+    <h1>Home</h1>
+  </main>
+</body>
+
+</html>
