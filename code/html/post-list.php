2cd93f654233d53910f664a8e4c7e7b7eec7e596
iansteyn <iansteyn03@gmail.com>
2025-02-24 21:43:54 -0800

    add post-list.php

---
+++ code/html/post-list.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../css/reset.css" />
+<link rel="stylesheet" href="../css/main.css" />
+<link rel="stylesheet" href="../post-list.css" />
+
+<article class="panel post-list">
+  <?php include "post-summary.html" ?>
+  <hr>
+  <?php include "post-summary.html" ?>
+  <hr>
+  <?php include "post-summary.html" ?>
+</article>
