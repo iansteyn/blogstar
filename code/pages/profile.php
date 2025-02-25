f9cfae2cf4359561263841a5492de0cf79d24b72
iansteyn <iansteyn03@gmail.com>
2025-02-24 21:44:24 -0800

    include post-list.php in profile.php to have a working example

---
+++ code/pages/profile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="../css/reset.css" />
+  <link rel="stylesheet" href="../css/main.css" />
+  <link rel="stylesheet" href="../css/profile.css" />
+  <link rel="stylesheet" href="../css/side-nav.css" />
+  <script src="../scripts/side-nav.js" defer></script>
+</head>
+
+<body>
+  <header>
+    <?php include "../html/side-nav.html" ?>
+  </header>
+  <main>
+    <h1>My Profile</h1>
+    <br> <!-- TODO remove -->
+    <?php include "../html/post-list.php" ?>
+  </main>
+</body>
+</html>
