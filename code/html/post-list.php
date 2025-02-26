c24135469372328e0817a722b5ed3da0f769ca6b
iansteyn <iansteyn03@gmail.com>
2025-02-26 11:03:17 -0800

    correctly link stylesheets

--- code/html/post-list.php
+++ code/html/post-list.php
@@ -1,7 +1,3 @@
-<link rel="stylesheet" href="../css/reset.css" />
-<link rel="stylesheet" href="../css/main.css" />
-<link rel="stylesheet" href="../post-list.css" />
-
 <article class="panel post-list">
   <?php include "post-summary.html" ?>
   <hr>
