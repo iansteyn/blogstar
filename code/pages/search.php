4cb1b76cf904a3995fbd9c61e45683a17cd373b1
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-02-28 12:07:48 -0800

    Add example posts and styling to search page

--- code/pages/search.php
+++ code/pages/search.php
@@ -7,23 +7,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="../css/reset.css" />
   <link rel="stylesheet" href="../css/main.css" />
-  <link rel="stylesheet" href="../css/home.css">
+  <link rel="stylesheet" href="../css/search.css">
   <link rel="stylesheet" href="../css/side-nav.css">
   <script src="../scripts/side-nav.js" defer></script>
 </head>
 
 <body>
-<header>
+  <header>
     <?php include "../html/side-nav.html" ?>
   </header>
   <main>
-    <header class="page-header home-page-header">
+    <header class="page-header">
         <h1>Search</h1>
-        <?php include "../html/search-bar.html" ?>
     </header>
     <div class="panel">
-      <div id="search-topics">Topics</div>
-      <div id="search-results">Search Results</div>
+      <section class="search">
+        <?php include "../html/search-bar.html" ?>
+      </section>
+      <section id="search-results" class="search-results">
+        <?php include "../html/post-summary.php" ?>
+        <?php include "../html/post-summary.php" ?>
+        <?php include "../html/post-summary.php" ?>
+      </section>
     </div>
   </main>
 </body>
