a31fff74440a565201a90380d264a4af1ce6dc09
iansteyn <iansteyn03@gmail.com>
2025-02-28 13:38:40 -0800

    tab event listener to tabs.js

--- code/scripts/tabs.js
+++ code/scripts/tabs.js
@@ -3,3 +3,21 @@
 Allows for tabs at the top of a page like
 home or profile to be toggled.
 -------------------------------------- */
+
+const tabs = document.querySelectorAll(".tab");
+const subpages = document.querySelectorAll(".post-list-container");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", ()=> {
+
+        for (let i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove("active");
+            subpages[i].classList.add("hidden");
+        }
+
+        tab.classList.add("active");
+        
+        const activeSubpage = document.getElementById(tab.value);
+        activeSubpage.classList.remove("hidden");
+    });
+});
