8e85194fc68e8f890199de6098ae7109d9fa6b5a
Sammie <scullysammie@gmail.com>
2025-02-27 21:02:29 -0800

    Added panel to the branch
    Sorry I forgot like the main bit

--- code/pages/create.php
+++ code/pages/create.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="../css/main.css">
   <link rel="stylesheet" href="../css/side-nav.css">
   <link rel="stylesheet" href="../css/create.css">
+  <script src="../scripts/side-nav.js" defer></script>
 </head>
 
 <body>
@@ -19,20 +20,17 @@
     <?php include "../html/side-nav.html" ?>
   </header>
   <main>
-    <h1 class = "page-title">Create your post<h1>
-    <div class = "panel">
-      <form action="../pages/create.php">
-        <textarea class = "blog-title" placeholder = "Write your title here!"></textarea>
-        <br>
-        <textarea class = "blog-text" placeholder = "Write your title here!"></textarea>
-        <div class="form-group">
-          <label for="blog-photo">Upload a photo</label>
-          <input type="file" id="blog-photo" name="blog-photo"/>
-        </div>
-        <input type="submit" value="Post">
-        <input type="submit" value="Discard">
-      </form>
-    </div>
+    <h1 class = "page-title">Create your post</h1>
+    <form class = "panel create-panel" action="../pages/create.php">
+      <textarea class = "blog-title" placeholder = "Write your title here!"></textarea>
+      <textarea class = "blog-text" placeholder = "Write your post here!"></textarea>
+      <div class="form-group">
+        <label for="blog-photo">Upload a photo</label>
+        <input type="file" id="blog-photo" name="blog-photo"/>
+      </div>
+      <input type="submit" value="Post">
+      <input type="submit" value="Discard">
+    </form>
   </main>
 </body>
 
