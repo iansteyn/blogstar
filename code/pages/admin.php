a198b09db8b395a3915019f4d94cceec989b5399
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-02-27 19:41:43 -0800

    Initial file setup

---
+++ code/pages/admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Admin Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="../css/reset.css" />
+  <link rel="stylesheet" href="../css/main.css" />
+  <link rel="stylesheet" href="../css/admin.css" />
+  <link rel="stylesheet" href="../css/side-nav.css">
+  <script src="../scripts/side-nav.js" defer></script>
+</head>
+
+<body>
+  <header>
+    <?php include "../html/side-nav.html" ?>
+  </header>
+  <main>
+    <header class="page-header">
+      <h1>Admin Dashboard</h1>
+    </header>
+    <div class="panel">
+
+    </div>
+  </main>
+</body>
+
+</html>
