39dcf3b686d07406911228af92512df1a7b6e12b
iansteyn <iansteyn03@gmail.com>
2025-02-28 12:39:58 -0800

    containers for different post-lists

--- code/pages/home.php
+++ code/pages/home.php
@@ -11,6 +11,7 @@
   <link rel="stylesheet" href="../css/reset.css">
   <link rel="stylesheet" href="../css/main.css">
   <link rel="stylesheet" href="../css/home.css">
+  <link rel="stylesheet" href="../css/post-list.css" />
   <link rel="stylesheet" href="../css/side-nav.css">
   <script src="../scripts/side-nav.js" defer></script>
 </head>
@@ -29,22 +30,14 @@
       <button class="tab" id="tab-popular">Popular</button>
       <button class="tab" id="tab-saved">Saved</button>
     </nav>
-    <div class="panel">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio elementum, eleifend elit cursus, mattis ante. Praesent mi eros, imperdiet nec mattis a, laoreet at lectus. Donec semper et justo ut iaculis. Mauris gravida et est nec commodo. Pellentesque auctor dui id urna aliquam, eu malesuada arcu fringilla. Vestibulum et augue vel lacus ullamcorper pellentesque. Sed sagittis orci at lacus laoreet sollicitudin. In metus sem, accumsan id ultricies quis, gravida eleifend erat. Etiam pulvinar vel tortor sed eleifend. Mauris ante nulla, mollis nec justo sed, porta pellentesque mi. Proin volutpat euismod placerat.
-      </p>
-      <p>
-        Curabitur eu sem dignissim mauris vulputate congue a vitae urna. Maecenas eu malesuada sem, sit amet tincidunt enim. Suspendisse non volutpat purus, cursus consectetur nibh. Cras hendrerit, erat eu viverra congue, erat justo vulputate ipsum, vitae pharetra risus sem a lorem. Fusce et molestie mauris. Nulla vitae mauris elementum, posuere ante sit amet, ultrices nulla. Ut pretium felis finibus, gravida arcu ut, malesuada purus. Proin pharetra feugiat porttitor. Cras id eleifend nisl, eget rhoncus ante. Suspendisse cursus laoreet mattis. Pellentesque vel posuere mi, id porta ligula. Sed vitae porttitor dui. Morbi eu mauris in dui tincidunt varius sit amet eget tellus. Aenean vitae lectus purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-      </p>
-      <p>
-        Sed non mollis mi, a hendrerit augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras id nisl egestas, semper neque ac, dapibus nibh. Nulla sit amet massa ac lacus gravida iaculis. Mauris varius tristique dui, at fringilla orci pretium ultrices. Mauris euismod pellentesque tristique. Vivamus dapibus ut diam eget sodales. Phasellus nec tellus et nisl consequat scelerisque eu id elit. Praesent tincidunt ut orci sed consectetur. Nulla maximus nisl id risus suscipit, a tempor velit malesuada. Cras pretium magna vitae metus pharetra, vel laoreet erat placerat. In consequat magna odio, nec varius lorem euismod ac.
-      </p>
-      <p>
-        Quisque semper sapien eu lectus lobortis accumsan. Aenean in odio nec diam fringilla malesuada. Donec dolor odio, ultrices quis felis nec, tempus pulvinar enim. Proin egestas non orci maximus tristique. In hac habitasse platea dictumst. Vivamus quis velit porta, maximus mauris eu, dignissim nunc. Duis nec eros est. Pellentesque at dolor eget enim aliquet blandit vel quis turpis.
-      </p>
-      <p>
-        Pellentesque blandit consequat felis, vel lobortis tortor porta eu. Aliquam id hendrerit dui. Donec tristique finibus quam, rutrum congue leo consequat non. Donec iaculis orci in tortor placerat malesuada. Nulla ut neque justo. Phasellus odio felis, vulputate at gravida id, viverra at ante. Morbi a sapien eu erat accumsan feugiat eu a mi. Suspendisse nisl augue, dapibus ut lectus nec, varius pellentesque dolor. Morbi ac est elementum, pharetra felis a, euismod est. Suspendisse felis est, gravida bibendum placerat at, sollicitudin sed justo. Phasellus varius dui sollicitudin diam imperdiet molestie. Vivamus sagittis turpis sed ante facilisis viverra. Integer in mollis libero. Etiam tempor erat eros, nec malesuada velit vestibulum id.
-      </p>
+    <div id="container-new">
+      <?php include "../html/post-list.php" ?>
+    </div>
+    <div id="container-popular" class="hidden">
+      <?php include "../html/post-list.php" ?>
+    </div>
+    <div id="container-saved" class="hidden">
+      <?php include "../html/post-list.php" ?>
     </div>
   </main>
 </body>
