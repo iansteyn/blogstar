2573708740f1181339a242c9b9ee95e5b2a4fb7e
Joy Musiel-Henseleit <joymusiel@gmail.com>
2025-02-27 20:52:39 -0800

    Added canvas for 'analytics' and additional semantic tags

--- code/pages/admin.php
+++ code/pages/admin.php
@@ -22,17 +22,41 @@
     </header>
     <article class="dashboard-grid">
       <section class="panel user-search" id="user-search">
-
+        <h2>User List</h2>
+        <div class="action-bar">
+          <?php include "../html/search-bar.html" ?>
+        </div>
       </section>
       <section class="panel search-topics" id="search-topics">
-
+        <h2>Site Search Topics</h2>
+        <div class="action-bar">
+          <!-- TODO: Add ability to add search topics -->
+        </div>
       </section>
       <section class="panel site-analytics" id="site-analytics">
-
+        <h2>Site Analytics</h2>
+        <canvas id="analytics" width="300" height="300"></canvas> <!-- temporary placeholder graphic -->
       </section>
     </article>
-
   </main>
+
+  <!-- temporary placeholder for site analytics -->
+  <script>
+    var ctx = document.getElementById("analytics").getContext("2d");
+    ctx.beginPath();
+    ctx.arc(150,150,125,0,2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo(25, 150);
+    ctx.lineTo(275, 150);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(150, 150);
+    ctx.lineTo(150, 25);
+    ctx.stroke();
+  </script>
 </body>
 
 </html>
