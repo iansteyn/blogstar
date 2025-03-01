aaaf365fe71fdb548fa16b1be994bc8df600aa9f
iansteyn <iansteyn03@gmail.com>
2025-03-01 13:02:47 -0800

    correctly implement subpages on profile
    ie add the tabs script and the right html tags. Profile tabs now work!

--- code/pages/profile.php
+++ code/pages/profile.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="../css/post-list.css" />
   <link rel="stylesheet" href="../css/side-nav.css" />
   <script src="../scripts/side-nav.js" defer></script>
+  <script src="../scripts/tabs.js" defer></script>
 </head>
 
 <body>
@@ -39,31 +40,40 @@
       </nav>
     </header>
 
-    <?php include "../html/post-list.php" ?>
-    <div class="panel" id="user-settings-panel">
-      <form id="user-settings" class="account-form" method="get" action="#">
-        <div class="form-group">
-          <label for="update-user-id">Update user id</label>
-          <input type="text" id="update-user-id" placeholder="Update your user id" />
+    <div class="subpage-group">
+      <div class="subpage" id="posts">
+        <?php include "../html/post-list.php" ?>
+      </div>
+      <div class="subpage" id="saved">
+        <?php include "../temporary/post-list-3.php" ?>
+      </div>
+      <div class="subpage" id="settings">
+        <div class="panel" id="user-settings-panel">
+          <form id="user-settings" class="account-form" method="get" action="#">
+            <div class="form-group">
+              <label for="update-user-id">Update user id</label>
+              <input type="text" id="update-user-id" placeholder="Update your user id" />
+            </div>
+            <div class="form-group">
+              <label for="update-password">Update password</label>
+              <input type="password" id="update-password" placeholder="Update your password" />
+            </div>
+            <div class="form-group">
+              <label for="confirm-update-password">Confirm password</label>
+              <input type="password" id="confirm-update-password" placeholder="Confirm your updated password" />
+            </div>
+            <div class="form-group">
+              <label for="update-profile-picture">Update profile picture</label>
+              <input type="file" id="update-profile-picture" accept="image/png, image/jpeg, image/jpg, image/gif" />
+            </div>
+            <div class="form-group">
+              <label for="edit-user-bio">Edit user bio</label>
+              <textarea id="edit-user-bio" maxlength="300" placeholder="Write something about yourself" rows="4"></textarea>
+            </div>
+            <button type="submit">Update user settings</button>
+          </form>
         </div>
-        <div class="form-group">
-          <label for="update-password">Update password</label>
-          <input type="password" id="update-password" placeholder="Update your password" />
-        </div>
-        <div class="form-group">
-          <label for="confirm-update-password">Confirm password</label>
-          <input type="password" id="confirm-update-password" placeholder="Confirm your updated password" />
-        </div>
-        <div class="form-group">
-          <label for="update-profile-picture">Update profile picture</label>
-          <input type="file" id="update-profile-picture" accept="image/png, image/jpeg, image/jpg, image/gif" />
-        </div>
-        <div class="form-group">
-          <label for="edit-user-bio">Edit user bio</label>
-          <textarea id="edit-user-bio" maxlength="300" placeholder="Write something about yourself" rows="4"></textarea>
-        </div>
-        <button type="submit">Update user settings</button>
-      </form>
+      </div>
     </div>
   </main>
 </body>
