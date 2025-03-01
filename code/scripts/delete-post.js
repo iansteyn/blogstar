75eb86b817224d31be1ba077100f462d631ca2c6
iansteyn <iansteyn03@gmail.com>
2025-03-01 15:46:33 -0800

    use function definition instead of assigning anonymous functions to consts
    Both ways work, but this way is more consistent with other files and lets us see the important code up top (since functions can be defined anywhere

--- code/scripts/delete-post.js
+++ code/scripts/delete-post.js
@@ -2,23 +2,25 @@ const submitButton = document.getElementById("submit-post-button");
 const discardButton = document.getElementById("discard-post-button");
 const createForm = document.getElementById("create-form");
 
-const submitPost = (event) => {
+if (submitButton && createForm) {
+    createForm.addEventListener("submit", submitPost); 
+  
+    if (discardButton) {
+      discardButton.addEventListener("click", discardPost);
+    }
+  }
+
+function submitPost(event) {
   event.preventDefault(); 
   window.location.href = "profile.php"; 
 };
 
-const discardPost = () => {
+function discardPost() {
   const confirmDiscard = confirm("Are you sure you want to discard this post?");
   if (confirmDiscard) {
     window.location.href = "profile.php"; 
   }
 };
 
-if (submitButton && createForm) {
-  createForm.addEventListener("submit", submitPost); 
 
-  if (discardButton) {
-    discardButton.addEventListener("click", discardPost);
-  }
-}
 
