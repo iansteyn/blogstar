17358f78f6075f478d154b561b04ef77de95fed2
iansteyn <iansteyn03@gmail.com>
2025-03-01 16:35:06 -0800

    add Welcome section

--- code/pages/about.php
+++ code/pages/about.php
@@ -22,6 +22,28 @@
     <header class="page-header">
       <h1>About</h1>
     </header>
+    <article class="panel">
+      <!-- about this website -->
+      <section>
+        <h2>Welcome to Our Site!</h2>
+        <p>
+          Our site is a personal blogging website, where you can create and read longform text posts. There's lots of different ways to find posts: search by keyword, and check out what's popular, and save your favorites! You can also engage with other bloggers by browsing their profiles and commenting on their posts.
+        </p>
+        <p>
+          This site was created as a term project for COSC 360 - Web Programming. We have learned a great deal in the process, and are very proud of what we have built.
+        </p>
+        <p>
+          <i>—The devs.</i>
+        </p>
+        <!-- <h3>Do I have to register an account?</h3>
+        <p>
+          You can browse posts without having an account, but you must register and log in to create your own posts and view the profiles of other users. Your personal information is kept secure, and we do not share it with anyone.
+        </p> -->
+      </section>
+      <!-- the team -->
+      <!-- github -->
+      <!-- attribution -->
+    </article>
   </main>
 </body>
 
