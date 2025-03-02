464cda7c5cdc5b4ecfb79e22a1f2a398213de3bf
iansteyn <iansteyn03@gmail.com>
2025-03-01 16:58:05 -0800

    add development information section

--- code/pages/about.php
+++ code/pages/about.php
@@ -11,6 +11,18 @@
   <link rel="stylesheet" href="../css/reset.css">
   <link rel="stylesheet" href="../css/main.css">
   <link rel="stylesheet" href="../css/side-nav.css">
+
+  <style>
+    .normal-list {
+      list-style: inside;
+      margin: 1em 0;
+      line-height: 1.2em;
+    }
+    h2 {
+      margin-bottom: 1rem;
+    }
+  </style>
+
   <script src="../scripts/side-nav.js" defer></script>
 </head>
 
@@ -35,11 +47,31 @@
         <p>
           <i>—The devs.</i>
         </p>
+      </section>
+      <section>
+        <h2>Development Information</h2>
+        <h3>The Team</h3>
+        <ul class="normal-list">
+          <li><a href="https://github.com/JoyMusiel" target="_blank">Joy Musiel-Henseleit</a></li>
+          <li><a href="https://github.com/SammieScully" target="_blank">Sammie Scully</a></li>
+          <li><a href="https://github.com/iansteyn" target="_blank">Ian Steyn</a></li>
+        </ul>
+        <h3>Code</h3>
+        <p>
+          You can find the source code for this website at <a href="https://github.com/iansteyn/cosc-360-project" target="_blank">our GitHub repository</a>.
+        </p>
+        <p>
+          We used no front-end frameworks. It's pure HTML/CSS/JS, hand-crafted!
+        </p>
+        <h3>Attribution</h3>
+        <p>
+          <!-- TODO -->
+        </p>
+      </section>
         <!-- <h3>Do I have to register an account?</h3>
         <p>
           You can browse posts without having an account, but you must register and log in to create your own posts and view the profiles of other users. Your personal information is kept secure, and we do not share it with anyone.
         </p> -->
-      </section>
       <!-- the team -->
       <!-- github -->
       <!-- attribution -->
