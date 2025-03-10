6969d5d49f73abad8c2fe6ec401ddb6027b9fbbf
iansteyn <iansteyn03@gmail.com>
2025-03-09 22:41:21 -0700

    delete outdated code

--- code/scripts/post-interaction.js
+++ code/scripts/post-interaction.js
@@ -51,29 +51,3 @@ function toggleButton(button) {
     button.classList.add('hidden');
     otherButton.classList.remove('hidden');
 }
-
-// const likeButton = document.getElementById('like-button');
-// const unlikeButton = document.getElementById('unlike-button');
-
-// unlikeButton.addEventListener('click', () => {
-//   unlikeButton.style.display = 'none';
-//   likeButton.style.display = 'inline-block';
-// });
-
-// likeButton.addEventListener('click', () => {
-//   likeButton.style.display = 'none';
-//   unlikeButton.style.display = 'inline-block';
-// });
-
-// const saveButton = document.getElementById('save-post-button');
-// const unsaveButton = document.getElementById('unsave-post-button');
-
-// unsaveButton.addEventListener('click', () => {
-//   unsaveButton.style.display = 'none';
-//   saveButton.style.display = 'inline-block';
-// });
-
-// saveButton.addEventListener('click', () => {
-//   saveButton.style.display = 'none';
-//   unsaveButton.style.display = 'inline-block';
-// });
