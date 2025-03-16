2105f5d796683c5fe8a79d87495520f7eda85e05
iansteyn <iansteyn03@gmail.com>
2025-03-15 18:03:40 -0700

    add more variables to User class

--- code/app/models/User.php
+++ code/app/models/User.php
@@ -2,6 +2,11 @@
 
 class User {
     public $username;
+    public $email;
+    public $password;
+    public $image; //is this the best name?
+    public $bio;
+    public $dateCreated;
 
     function save() {
         // TODO database stuff here
