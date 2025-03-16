d26ab777ab77f4920a4f050440d42c93d2a85b05
iansteyn <iansteyn03@gmail.com>
2025-03-15 18:33:23 -0700

    add PostModel skeleton

---
+++ code/app/models/PostModel.php
@@ -0,0 +1,18 @@
+<?php
+
+class PostModel {
+    function getPostById($id): Post {
+        //TODO get post object from db
+    }
+
+    function getRecentPosts(): array {
+        //TODO return array of recent posts from db
+    }
+
+    function getPopularPosts(): array {
+        //TODO return array of popular posts from db
+
+    }
+}
+
+?>
