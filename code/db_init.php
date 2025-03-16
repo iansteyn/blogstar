b81c885a03770d53bbd21f384428ccc7e38ba76c
Sammie <scullysammie@gmail.com>
2025-03-16 13:20:09 -0700

    Changing to PDO after merge
    Merged Ians very scary block of server code into my branch from main
    
    Placed databse code into his db init file
    
    Removed my file
    
    Converted to pdo

--- code/db_init.php
+++ code/db_init.php
@@ -2,5 +2,21 @@
     function getDataBaseConnection() {
         // TODO: @SammieScully add db connection logic here
         // should return a PDO object I believe
+
+        $db_server = "cosc-360-project.local";
+        $db_user = "root";
+        $db_pass = "";
+        $db_name = "practice_wp2";
+        $conn = "";
+
+        # cosc-360-project.local/db_init.php
+        try {
+            $conn = new PDO("mysql:host=$db_server; dbname=$db_name", $db_user, $db_pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Hi Sammie :)";
+            return $conn;
+        } catch (PDOException $e) {
+            die("Not connected :(" . $e->getMessage());
+        }
     }
 ?>
