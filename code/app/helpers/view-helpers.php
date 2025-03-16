7034e6f224221eeb3b534c515e4e07311b112273
iansteyn <iansteyn03@gmail.com>
2025-03-16 11:31:17 -0700

    add meta viewport tag to generateDocumentHead

--- code/app/helpers/view-helpers.php
+++ code/app/helpers/view-helpers.php
@@ -26,6 +26,7 @@ function generateDocumentHead(string $title, array $extraStylesheets, array $ext
         <html lang="en" class="hidden">
         <head>
           <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
 
           <title>
             $title | Our Site
