da6835f13d204596ea344e3e0f08184931479eb0
iansteyn <iansteyn03@gmail.com>
2025-03-16 09:59:08 -0700

    add generateDocumentHead function to view-helpers

--- code/app/helpers/view-helpers.php
+++ code/app/helpers/view-helpers.php
@@ -7,6 +7,30 @@
  * -----------------------------------------------------------------------------------
  */
 
+function generateDocumentHead(string $title, array $extraStylesheetLinks, array $extraScriptLinks) {
+    return <<<HTML
+        <!DOCTYPE html>
+        <html lang="en" class="hidden">
+        <head>
+          <meta charset="UTF-8">
+
+          <title>
+            $title | Our Site
+          </title>
+
+          <link rel="stylesheet" href="/css/reset.css">
+          <link rel="stylesheet" href="/css/main.css">
+          <link rel="stylesheet" href="/css/side-nav.css">
+          $extraStylesheetLinks
+
+          <!-- TODO insert favicon -->
+
+          <script src="/scripts/side-nav.js" defer></script>
+          $extraScriptLinks
+        </head>
+    HTML;
+}
+
 function isTabActive($tab, $activeTab) {
     if($tab == $activeTab) {
         return "active";
