961b1143f3ff936ed7bf8ab2fdf295ea9019e1e9
iansteyn <iansteyn03@gmail.com>
2025-03-16 17:16:36 -0700

    replace post-summary dummy data with php variables

--- code/app/components/post-summary.php
+++ code/app/components/post-summary.php
@@ -1,11 +1,27 @@
+<?php 
+/*
+post-summary.php expects the following variables:
+- array $postData
+- bool $isLiked
+- bool $isSaved
+*/
+
+//TODO
+// - link to actual post properly
+    // See concept of 'slug urls'
+// - photo
+
+require_once __DIR__."/../helpers/view-helpers.php"
+?>
+
 <article class="post-summary">
   <div class="post-summary-text">
     <h2 class="post-summary-title">
-      <a href="/specific-post">Post Title that is reasonably long I suppose</a>
+      <a href="/specific-post"><?= $postData['title'] ?></a>
     </h2>
-    <?php include "posting-info.php" ?>
+    <?= generatePostingInfo($postData['username'], $postData['date']) ?>
     <p class="blog-start">
-      <a href="/specific-post">Let's pretend this is the first twoish lines of a an article. Lorem impsum dolor sit amet, consectetur adipiscing eli Morbi ac odio elementum, eleifend elit cursus, mattis ante. Praesent mi eros, i...</a>
+      <a href="/specific-post"><?= $postData['textContent'] ?></a>
     </p>
   </div>
 
@@ -14,23 +30,23 @@
 
     <div class="post-summary-button-group button-group-icon-only">
       <div class="interact-buttons">
-        <button class="togglable-post-button button-icon-only" title="Like">
+        <button title="Like" class="<?= hiddenIf($isLiked) ?> togglable-post-button button-icon-only">
           <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
             <use href="../vector-icons/icons.svg#icon-like-unfilled"></use>
           </svg>
         </button>
-        <button class="togglable-post-button togglable-post-button-active button-icon-only hidden" title="Unlike">
+        <button title="Unlike" class="<?= hiddenIf(!$isLiked) ?> togglable-post-button togglable-post-button-active button-icon-only">
           <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
             <use href="../vector-icons/icons.svg#icon-like-filled"></use>
           </svg>
         </button>
-    
-        <button class="togglable-post-button button-icon-only" title="Save">
+        
+        <button title="Save" class="<?= hiddenIf($isSaved) ?> togglable-post-button button-icon-only">
           <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
             <use href="../vector-icons/icons.svg#icon-save-unfilled"></use>
           </svg>
         </button>
-        <button class="togglable-post-button togglable-post-button-active button-icon-only hidden" title="Unsave">
+        <button title="Unsave" class="<?= hiddenIf(!$isSaved) ?> togglable-post-button togglable-post-button-active button-icon-only hidden">
           <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
             <use href="../vector-icons/icons.svg#icon-save-filled"></use>
           </svg>
@@ -38,12 +54,12 @@
       </div>
 
       <div class="modify-buttons">
-        <button class="button-icon-only edit-post-button" title="Edit">
+        <button title="Edit" class="button-icon-only edit-post-button">
           <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
             <use href="../vector-icons/icons.svg#icon-edit"></use>
           </svg>
         </button>
-        <button class="button-icon-only delete-post-button" title="Delete">
+        <button title="Delete" class="button-icon-only delete-post-button">
           <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
             <use href="../vector-icons/icons.svg#icon-delete"></use>
           </svg>
