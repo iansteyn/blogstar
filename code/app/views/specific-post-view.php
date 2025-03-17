84d9dc00297e026762b48591e5cf2e5cdcadb58a
iansteyn <iansteyn03@gmail.com>
2025-03-16 18:12:28 -0700

    Replace specific-post-view dummy data with PHP variables

--- code/app/views/specific-post-view.php
+++ code/app/views/specific-post-view.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
 
   <title>
-    Blog Post | Our Site
+    <?= $postData['title'] ?> | Our Site
   </title>
 
   <link rel="stylesheet" href="/css/reset.css">
@@ -34,57 +34,39 @@
     <article class="panel blog-panel">
       <header>
         <h1 class="blog-title">
-          I took this photo in New Zealand :)
+          <?= $postData['title'] ?>
         </h1>
-        <?php include __DIR__."/../components/posting-info.php" ?>
+        <?= generatePostingInfo($postData['username'], $postData['date']) ?>
       </header>
       <img class="blog-photo" src="../photo/sunrise.jpg" alt="A photo of a sunrise looking over a beach.">
       <div class="blog-text">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac odio elementum, eleifend elit cursus, mattis ante. Praesent mi eros, imperdiet nec mattis a, laoreet at lectus. Donec semper et justo ut iaculis. Mauris gravida et est nec commodo. Pellentesque auctor dui id urna aliquam, eu malesuada arcu fringilla. Vestibulum et augue vel lacus ullamcorper pellentesque. Sed sagittis orci at lacus laoreet sollicitudin. In metus sem, accumsan id ultricies quis, gravida eleifend erat. Etiam pulvinar vel tortor sed eleifend. Mauris ante nulla, mollis nec justo sed, porta pellentesque mi. Proin volutpat euismod placerat.
-        </p>
-        <p>
-          Curabitur eu sem dignissim mauris vulputate congue a vitae urna. Maecenas eu malesuada sem, sit amet tincidunt enim. Suspendisse non volutpat purus, cursus consectetur nibh. Cras hendrerit, erat eu viverra congue, erat justo vulputate ipsum, vitae pharetra risus sem a lorem. Fusce et molestie mauris. Nulla vitae mauris elementum, posuere ante sit amet, ultrices nulla. Ut pretium felis finibus, gravida arcu ut, malesuada purus. Proin pharetra feugiat porttitor. Cras id eleifend nisl, eget rhoncus ante. Suspendisse cursus laoreet mattis. Pellentesque vel posuere mi, id porta ligula. Sed vitae porttitor dui. Morbi eu mauris in dui tincidunt varius sit amet eget tellus. Aenean vitae lectus purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-        </p>
-        <p>
-          Sed non mollis mi, a hendrerit augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras id nisl egestas, semper neque ac, dapibus nibh. Nulla sit amet massa ac lacus gravida iaculis. Mauris varius tristique dui, at fringilla orci pretium ultrices. Mauris euismod pellentesque tristique. Vivamus dapibus ut diam eget sodales. Phasellus nec tellus et nisl consequat scelerisque eu id elit. Praesent tincidunt ut orci sed consectetur. Nulla maximus nisl id risus suscipit, a tempor velit malesuada. Cras pretium magna vitae metus pharetra, vel laoreet erat placerat. In consequat magna odio, nec varius lorem euismod ac.
-        </p>
-        <p>
-          Quisque semper sapien eu lectus lobortis accumsan. Aenean in odio nec diam fringilla malesuada. Donec dolor odio, ultrices quis felis nec, tempus pulvinar enim. Proin egestas non orci maximus tristique. In hac habitasse platea dictumst. Vivamus quis velit porta, maximus mauris eu, dignissim nunc. Duis nec eros est. Pellentesque at dolor eget enim aliquet blandit vel quis turpis.
-        </p>
-        <p>
-          Pellentesque blandit consequat felis, vel lobortis tortor porta eu. Aliquam id hendrerit dui. Donec tristique finibus quam, rutrum congue leo consequat non. Donec iaculis orci in tortor placerat malesuada. Nulla ut neque justo. Phasellus odio felis, vulputate at gravida id, viverra at ante. Morbi a sapien eu erat accumsan feugiat eu a mi. Suspendisse nisl augue, dapibus ut lectus nec, varius pellentesque dolor. Morbi ac est elementum, pharetra felis a, euismod est. Suspendisse felis est, gravida bibendum placerat at, sollicitudin sed justo. Phasellus varius dui sollicitudin diam imperdiet molestie. Vivamus sagittis turpis sed ante facilisis viverra. Integer in mollis libero. Etiam tempor erat eros, nec malesuada velit vestibulum id.
-        </p>
-        <p>
-          Integer sit amet neque non est dapibus auctor. In sagittis justo ut luctus vehicula. Sed aliquam, nibh vel venenatis sagittis, est elit lobortis justo, sed rutrum orci risus ac eros. Praesent laoreet magna sit amet dui volutpat, at faucibus nisl ullamcorper. Suspendisse eu orci augue. Vestibulum posuere euismod nisl, et maximus diam feugiat non. Sed mattis nisi semper felis consequat, eu vehicula augue interdum. Nullam pulvinar, arcu ut blandit rutrum, nisi libero tempus libero, eu pharetra ante ligula non est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi lacinia eros sed dolor ullamcorper, quis facilisis sem hendrerit. Mauris velit urna, faucibus a tortor eu, lobortis malesuada urna. Duis et elementum urna. Suspendisse malesuada dapibus ipsum eget facilisis. Vivamus nec dapibus magna. Duis libero magna, egestas vel risus accumsan, pulvinar euismod sem.
-        </p>
-        <p>
-          Proin scelerisque gravida lacinia. Aenean interdum leo ut lorem sagittis, tempor sagittis felis tempor. Vestibulum ac ante nec tortor convallis pellentesque non non ante. In porttitor sapien eu lectus maximus, a vestibulum mauris eleifend. Duis varius lacus at turpis suscipit tempor. Donec nec dictum ante. In tincidunt volutpat risus, pretium fringilla massa. Nulla sodales tincidunt lorem in fringilla. Nulla eget sem vel nisi dignissim viverra.
-        </p>
+        <?php foreach ($postData['paragraphs'] as $paragraph) {
+            echo "<p>$paragraph</p>";
+        } ?>
       </div>
     </article>
 
     <div class="interaction-bar">
-      <button class="interaction-button togglable-post-button" type="button" title="Like">
+      <button class="<?= hiddenIf($isLiked) ?> interaction-button togglable-post-button" type="button" title="Like">
         <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
           <use href="../vector-icons/icons.svg#icon-like-unfilled"></use> 
         </svg>
         Like
       </button>
-      <button class="interaction-button togglable-post-button togglable-post-button-active hidden" type="button" title="Unlike">
+      <button class="<?= hiddenIf(!$isLiked) ?> interaction-button togglable-post-button togglable-post-button-active" type="button" title="Unlike">
         <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
           <use href="../vector-icons/icons.svg#icon-like-filled"></use> 
         </svg>
         Liked
       </button>
 
-      <button class="interaction-button togglable-post-button" type="button" title="Save">
+      <button class="<?= hiddenIf($isSaved) ?> interaction-button togglable-post-button" type="button" title="Save">
         <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
           <use href="../vector-icons/icons.svg#icon-save-unfilled"></use> 
         </svg>
         Save
       </button>
-      <button class="interaction-button togglable-post-button togglable-post-button-active hidden" type="button" title="Unsave">
+      <button class="<?= hiddenIf(!$isSaved) ?> interaction-button togglable-post-button togglable-post-button-active" type="button" title="Unsave">
         <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
           <use href="../vector-icons/icons.svg#icon-save-filled"></use> 
         </svg>
