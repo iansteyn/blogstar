902b49ab6d876b5b204ac1a67821bc92908ca5e5
iansteyn <iansteyn03@gmail.com>
2025-03-19 16:57:37 -0700

    link to post from summary

--- code/app/components/post-summary.php
+++ code/app/components/post-summary.php
@@ -7,27 +7,36 @@ post-summary.php expects the following variables:
 */
 
 //TODO
-// - link to actual post properly
     // See concept of 'slug urls'
 // - photo
 // - limit post_body to first X amount of characters
 
-require_once __DIR__."/../helpers/view-helpers.php"
+require_once __DIR__."/../helpers/view-helpers.php";
+
+$blogPostLink = "blog-post/".$postData['post_id'];
 ?>
 
 <article class="post-summary">
   <div class="post-summary-text">
     <h2 class="post-summary-title">
-      <a href="/specific-post"><?= $postData['post_title'] ?></a>
+      <a href=<?= $blogPostLink ?>>
+        <?= $postData['post_title'] ?>
+      </a>
     </h2>
+
     <?= generatePostingInfo($postData['username'], $postData['post_date']) ?>
+
     <p class="blog-start">
-      <a href="/specific-post"><?= $postData['post_body'] ?></a>
+      <a href=<?= $blogPostLink ?>>
+        <?= $postData['post_body'] ?>
+      </a>
     </p>
   </div>
 
   <div class="post-summary-non-text">
-    <a href="/specific-post"><img class="post-summary-img" src="../photo/knit.png" alt="Blog post photo"/></a>
+    <a href=<?= $blogPostLink ?>>
+        <img class="post-summary-img" src="../photo/knit.png" alt="Blog post photo"/>
+    </a>
 
     <div class="post-summary-button-group button-group-icon-only">
       <div class="interact-buttons">
