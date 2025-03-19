7c7ba6c69616a16a2e60186cee24fd6055b4ef26
iansteyn <iansteyn03@gmail.com>
2025-03-18 21:05:09 -0700

    update path for db_connect in index.php

--- code/public/index.php
+++ code/public/index.php
@@ -5,7 +5,7 @@ This is the website's "root".
 Here, we set-up URL routing, so that all of our pages can be viewed without needing the whole URL.
 */
 
-include __DIR__.'/../db_connect.php';
+include __DIR__.'/../db_config/db_connect.php';
 $db = getDatabaseConnection();
 
 include __DIR__.'/../app/controllers/PagesController.php';
