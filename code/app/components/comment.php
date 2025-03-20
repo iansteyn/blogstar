5c06f5039bcb946ff37d85727b792b4b25f314d1
iansteyn <iansteyn03@gmail.com>
2025-03-19 17:13:12 -0700

    move comment to its own component file

---
+++ code/app/components/comment.php
@@ -0,0 +1,20 @@
+<div class = "specific-comment-container">
+  <header>
+    <?= generatePostingInfo("sadiesmith", "2015-07-08")?>
+    <div class="button-group-icon-only">
+      <button class="button-icon-only" title="Edit">
+        <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
+          <use href="../vector-icons/icons.svg#icon-edit"></use>
+        </svg>
+      </button>
+      <button class="button-icon-only" id="delete-comment-button" title="Delete">
+        <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
+          <use href="../vector-icons/icons.svg#icon-delete"></use>
+        </svg>
+      </button>
+    </div>
+  </header>
+  <div class = "comment-text">
+    <p>Hi Sadie, that is a really beautiful photo of New Zealand. Where exactly was it taken? I'm going there in the fall!</p>
+  </div>
+</div>
--- code/app/views/specific-post-view.php
+++ code/app/views/specific-post-view.php
@@ -84,26 +84,8 @@
         Comments
       </h2>
     </header>
-    <div class = "specific-comment-container">
-      <header>
-        <?php include __DIR__."/../components/posting-info.php" ?>
-        <div class="button-group-icon-only">
-          <button class="button-icon-only" title="Edit">
-            <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
-              <use href="../vector-icons/icons.svg#icon-edit"></use>
-            </svg>
-          </button>
-          <button class="button-icon-only" id="delete-comment-button" title="Delete">
-            <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
-              <use href="../vector-icons/icons.svg#icon-delete"></use>
-            </svg>
-          </button>
-        </div>
-      </header>
-      <div class = "comment-text">
-        <p>Hi Sadie, that is a really beautiful photo of New Zealand. Where exactly was it taken? I'm going there in the fall!</p>
-      </div>
-    </div>
+    
+    <?php include __DIR__."/../components/comment.php" ?>
 
     <div class = "specific-comment-container">
       <form method = "GET">
