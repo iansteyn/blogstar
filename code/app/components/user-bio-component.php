5294b242879ed883f197170774c4666951d4910d
Sammie <scullysammie@gmail.com>
2025-03-21 14:57:21 -0700

    Leaving hard-coded spooky

--- code/app/components/user-bio-component.php
+++ code/app/components/user-bio-component.php
@@ -1,16 +1,17 @@
 <?php 
+/*
+user-bio-component.php expects the following variable:
+- array $userData
+*/
 require_once __DIR__."/../helpers/view-helpers.php";
 
-  $username = $userData['username'] ?? 'Unknown User';
-  $bio = $userData['user_bio'] ?? 'This user has not added a bio yet.';
-  $profilePicture = $userData['profile_picture'] ?? '../photo/default-profile.jpg';
 ?>
   <body>
     <div class="sidebar-profile">
-    <img class="user-image" src="<?= htmlspecialchars($profilePicture)?>" alt="Profile picture of <?= htmlspecialchars($username) ?>">
+    <img class="user-image" src="../photo/sadie-smith.jpg" alt="Profile picture of <?= htmlspecialchars($userData['username']) ?>">
         <div class="user-info">
-            <h2 class="user-name"><?= htmlspecialchars($username)?></h2>
-            <p class="user-bio"><?= nl2br(htmlspecialchars($bio))?></p>
+            <h2 class="user-name"><?= htmlspecialchars($userData['username'])?></h2>
+            <p class="user-bio"><?= nl2br(htmlspecialchars($userData['user_bio']))?></p>
         </div>
     </div>
 
