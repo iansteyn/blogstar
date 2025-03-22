1c266d70c2ed59d1a8ed7e0204c71da9898a8773
iansteyn <iansteyn03@gmail.com>
2025-03-21 17:58:03 -0700

    remove uneccessary tags

--- code/app/components/user-bio-component.php
+++ code/app/components/user-bio-component.php
@@ -6,14 +6,11 @@ user-bio-component.php expects the following variable:
 require_once __DIR__."/../helpers/view-helpers.php";
 $userData = sanitizeData($userData);
 ?>
-  <body>
-    <div class="sidebar-profile">
-    <img class="user-image" src="../photo/sadie-smith.jpg" alt="Profile picture of <?= $userData['username'] ?>">
-        <div class="user-info">
-            <h2 class="user-name"><?= $userData['username']?></h2>
-            <p class="user-bio"><?= nl2br($userData['user_bio'])?></p>
-        </div>
-    </div>
 
-  </body>
-</html>
+<div class="sidebar-profile">
+    <img class="user-image" src="../photo/sadie-smith.jpg" alt="Profile picture of <?= $userData['username'] ?>">
+    <div class="user-info">
+        <h2 class="user-name"><?= $userData['username'] ?></h2>
+        <p class="user-bio"><?= nl2br($userData['user_bio']) ?></p>
+    </div>
+</div>
