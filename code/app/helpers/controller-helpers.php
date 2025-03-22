f2f884ca94ef665e70928888e2340b1cdc239339
iansteyn <iansteyn03@gmail.com>
2025-03-22 16:37:10 -0700

    add controller-helpers.php

---
+++ code/app/helpers/controller-helpers.php
@@ -0,0 +1,9 @@
+<?php
+/* controller-helpers.php
+ * -------------------------------------------------------------------------------------
+ * Contains utility functions for controllers.
+ * Include this file at the top of a controller if you want to use any of these methods.
+ * -------------------------------------------------------------------------------------
+ */
+
+ ?>
