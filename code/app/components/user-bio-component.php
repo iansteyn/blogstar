25555b2bea5784d82e29b16284b5962c55a364d8
iansteyn <iansteyn03@gmail.com>
2025-03-21 17:55:22 -0700

    use sanitizeData in user-bio-component

--- code/app/components/user-bio-component.php
+++ code/app/components/user-bio-component.php
@@ -1,17 +1,17 @@
 <?php 
 /*
 user-bio-component.php expects the following variable:
-- array $userData
+- array $userData with keys username, user_bio, profile_picture
 */
 require_once __DIR__."/../helpers/view-helpers.php";
-
+$userData = sanitizeData($userData);
 ?>
   <body>
     <div class="sidebar-profile">
-    <img class="user-image" src="../photo/sadie-smith.jpg" alt="Profile picture of <?= htmlspecialchars($userData['username']) ?>">
+    <img class="user-image" src="../photo/sadie-smith.jpg" alt="Profile picture of <?= $userData['username'] ?>">
         <div class="user-info">
-            <h2 class="user-name"><?= htmlspecialchars($userData['username'])?></h2>
-            <p class="user-bio"><?= nl2br(htmlspecialchars($userData['user_bio']))?></p>
+            <h2 class="user-name"><?= $userData['username']?></h2>
+            <p class="user-bio"><?= nl2br($userData['user_bio'])?></p>
         </div>
     </div>
 
