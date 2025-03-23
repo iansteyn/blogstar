56a2c93d0638b8f6b6de728fe9e0e231ac6390b5
Sammie <scullysammie@gmail.com>
2025-03-22 19:22:41 -0700

    Passing images to post summaries
    Removed hard coded pictures and replaced with necessary text

--- code/app/components/post-summary-component.php
+++ code/app/components/post-summary-component.php
@@ -36,7 +36,9 @@ $postBodyExcerpt = substr($postData['post_body'], 0, 300) . "...";
 
   <div class="post-summary-non-text">
     <a href=<?= $blogPostLink ?>>
-        <img class="post-summary-img" src="../photo/knit.png" alt="Blog post photo"/>
+      <?php if (!empty($postData['post_image'])): ?>
+        <img class="post-summary-img" src="<?= generatePostSummaryImages($postData) ?>" alt="Photo associated with this blog post"/>
+      <?php endif; ?>
     </a>
 
     <div class="post-summary-button-group button-group-icon-only">
