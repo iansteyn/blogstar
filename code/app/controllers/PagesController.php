710100c54a17bfb2a423f4059ddcd83b3c4367b4
iansteyn <iansteyn03@gmail.com>
2025-03-22 19:34:39 -0700

    Add helper function setLikeAndSaveStatus to PagesController

--- code/app/controllers/PagesController.php
+++ code/app/controllers/PagesController.php
@@ -13,16 +13,30 @@ class PagesController {
         $this->likeModel = new likeModel($db);
     }
 
+    /**
+     * Helper method; adds or sets `'is_liked'` and `'is_saved'` in the given `postData` array.
+     * @param array &$postData post data array passed by reference
+     * @return void
+     */
+    private function setLikeAndSaveStatus(array &$postData): void {
+        $postData['is_liked'] = $this->likeModel->userHasLikedPost($_SESSION['username'], $postData['post_id']);
+        $postData['is_saved'] = $this->saveModel->userHasSavedPost($_SESSION['username'], $postData['post_id']);
+    }
+
     public function home() {
         $activeTab = $_GET['tab'] ?? "recent";
         $recentPostsData = $this->postModel->getRecentPosts();
+        $savedPostsData = $this->postModel->getSavedPosts($_SESSION['username']);
 
         foreach ($recentPostsData as &$postData) {
-            $postData['is_liked'] = $this->likeModel->userHasLikedPost($_SESSION['username'], $postData['post_id']);
-            $postData['is_saved'] = $this->saveModel->userHasSavedPost($_SESSION['username'], $postData['post_id']);
+            $this->setLikeAndSaveStatus($postData);
         }
+        foreach ($savedPostsData as &$postData) {
+            $this->setLikeAndSaveStatus($postData);
+        }
+        unset($postData); //required to remove the &reference binding
 
-        // This view uses: $activeTab, $recentPostsData
+        // This view uses: $activeTab, $recentPostsData, $savedPostsData
         require __DIR__.'/../views/home-view.php';
     }
 }
