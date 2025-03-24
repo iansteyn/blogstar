66686dc5b07a7c32ffe03f6b4310782207ca8434
iansteyn <iansteyn03@gmail.com>
2025-03-23 18:04:14 -0700

    remove profile() from userController

--- code/app/controllers/UserController.php
+++ code/app/controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
-include __DIR__.'/../models/UserModel.php';
-include __DIR__.'/../authentication/AuthService.php';
+require_once __DIR__.'/../models/UserModel.php';
+require_once __DIR__.'/../authentication/AuthService.php';
 
 class UserController {
     private $userModel;
@@ -9,15 +9,6 @@ class UserController {
     public function __construct(PDO $db) {
         $this->userModel = new UserModel($db);
     }
-
-    function profile() {
-        $userData = $this->userModel->getUserByUsername($_SESSION['username']);
-        AuthService::requireAuth(['registered','admin']);
-
-        // This view uses: $userData
-        require __DIR__.'/../views/profile-view.php';
-    }
-    
 
     public function register() {
         // If form is not submitted, just display the page:
