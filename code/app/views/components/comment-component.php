dfd01785fb802e04fea77b4e2da1f3a649eaebc7
Sammie <scullysammie@gmail.com>
2025-03-23 17:05:25 -0700

    Hiding edit and delete buttons from users for comments

--- code/app/views/components/comment-component.php
+++ code/app/views/components/comment-component.php
@@ -11,16 +11,22 @@
   <header>
     <?= generatePostingInfo($comment['username'], $comment['comment_date'])?>
     <div class="button-group-icon-only">
-      <button class="button-icon-only" title="Edit">
-        <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
-          <use href="../vector-icons/icons.svg#icon-edit"></use>
-        </svg>
-      </button>
-      <button class="button-icon-only" id="delete-comment-button" title="Delete">
-        <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
-          <use href="../vector-icons/icons.svg#icon-delete"></use>
-        </svg>
-      </button>
+      <?php if ($_SESSION['username'] === $comment['username']): ?>
+        <button class="button-icon-only" title="Edit">
+          <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
+            <use href="../vector-icons/icons.svg#icon-edit"></use>
+          </svg>
+        </button>
+      <?php endif; ?>
+      <?php if ($_SESSION['username'] === $comment['username'] || $_SESSION['role'] === 'admin'): ?>
+        <form method="POST" action="/comment/delete/<?= $comment['comment_id'] ?>">
+          <button class="button-icon-only" id="delete-comment-button" title="Delete">
+            <svg class="icon-inline" preserveAspectRatio="xMidYMid meet">
+              <use href="../vector-icons/icons.svg#icon-delete"></use>
+            </svg>
+          </button>
+        </form>
+      <?php endif; ?>
     </div>
   </header>
   <div class = "comment-text">
