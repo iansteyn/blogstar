7f7ee3c6cdf03105fddb80a29c3442ff83b39737
iansteyn <iansteyn03@gmail.com>
2025-03-23 22:36:59 -0700

    add LikeModel, SaveModel, PostModel to Profile controller

--- code/app/controllers/ProfileController.php
+++ code/app/controllers/ProfileController.php
@@ -1,23 +1,50 @@
 <?php
 
+require_once __DIR__.'/../models/PostModel.php';
+require_once __DIR__.'/../models/SaveModel.php';
 require_once __DIR__.'/../models/UserModel.php';
+require_once __DIR__.'/../models/LikeModel.php';
+require_once __DIR__.'/../helpers/controller-helpers.php';
 require_once __DIR__.'/../authentication/AuthService.php';
 
 class ProfileController {
     private $userModel;
+    private $postModel;
+    private $saveModel;
+    private $likeModel;
+
 
     public function __construct(PDO $db) {
         $this->userModel = new UserModel($db);
+        $this->postModel = new PostModel($db);
+        $this->saveModel = new saveModel($db);
+        $this->likeModel = new likeModel($db);
     }
 
-    function profile() {
+    function posts(?string $username = null) {
         AuthService::requireAuth(['registered','admin']);
 
-        $userData = $this->userModel->getUserByUsername($_SESSION['username']);
+        $username = $username ?? $_SESSION['username']; //use current user if no other user is provided
+
+        $userData = $this->userModel->getUserByUsername($username);
         $isLoggedIn = AuthService::isLoggedIn();
         $isAdmin = AuthService::isAdmin();
 
+        // $postDataList = $this->postModel->getUserPosts($username);
+        // foreach ($postDataList as &$postData) {
+        //     $postData = setLikeAndSaveStatus($postData, $isLoggedIn, $this->likeModel, $this->saveModel);
+        // }
+        // unset($postData);
+
         // This view uses: $userData, $isLoggedIn, $isAdmin
         require __DIR__.'/../views/profile-view.php';
+    }
+
+    function saved() {
+
+    }
+
+    function settings() {
+
     }
 }
