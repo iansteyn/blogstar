d1a678bc22521b9f8f3c953e66b1b9d7cb49562d
iansteyn <iansteyn03@gmail.com>
2025-03-26 18:22:02 -0700

    Show search results post-list in search-view

--- code/app/views/search-view.php
+++ code/app/views/search-view.php
@@ -18,22 +18,38 @@ echo generateDocumentHead(
 
 <body>
   <header>
-    <?php require_once __DIR__."/components/side-nav-component.php" ?>
+    <?php
+        //this component uses: $isAdmin, $isLoggedIn
+        require_once __DIR__."/components/side-nav-component.php"
+    ?>
   </header>
   <main>
     <header class="page-header">
         <h1>Search blog posts</h1>
     </header>
-    <div class="panel">
+    <div class="panel post-list">
       <?php
           $searchAction = '/search';
           // this component uses: $searchAction
           include __DIR__."/components/search-bar-component.php";
       ?>
-      <div class='help-message'>
-        You can search for blog posts using one or more keywords, seperated by spaces.
-        Press "Enter" or click the search icon to submit your query.
-      </div>
+      <?php if (! $showResults): ?>
+        <div class='search-message help-message'>
+            You can search for blog posts using one or more keywords, seperated by spaces.
+            Press "Enter" or click the search icon to submit your query.
+        </div>
+      <?php else: ?>
+        <h3 class='search-message'>
+          <?= empty($postDataList) ? ':( No' :  'Showing' ?>
+          results for: TODO insert terms var
+        </h3>
+        <?php
+            foreach ($postDataList as $postData) {
+                // This component uses: $postData
+                include __DIR__."/components/post-summary-component.php";
+            }
+        ?>
+      <?php endif; ?>
     </div>
 
   </main>
