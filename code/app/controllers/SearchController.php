bbf679e88b3991e8bfea9090130f64c330fddf8e
iansteyn <iansteyn03@gmail.com>
2025-03-26 18:21:46 -0700

    add likeModel, saveModel and use getSearchedPosts in search()

--- code/app/controllers/SearchController.php
+++ code/app/controllers/SearchController.php
@@ -1,22 +1,41 @@
 <?php
 
 require_once __DIR__.'/../models/PostModel.php';
+require_once __DIR__.'/../models/LikeModel.php';
+require_once __DIR__.'/../models/SaveModel.php';
 require_once __DIR__.'/../authentication/AuthService.php';
 
 class SearchController {
     private $postModel;
+    private $likeModel;
+    private $saveModel;
 
     public function __construct(PDO $db) {
         $this->postModel = new PostModel($db);
+        $this->likeModel = new LikeModel($db);
+        $this->saveModel = new SaveModel($db);
     }
 
     public function search() {
-        // TODO for milestone 4: search functionality
-
         $isLoggedIn = AuthService::isLoggedIn();
         $isAdmin = AuthService::isAdmin();
+        $showResults = false;
+        $postDataList = [];
 
-        // This view uses: $isLoggedIn, $isAdmin
+        if (isset($_GET['terms'])) {
+            $showResults = true;
+
+            $keywords = explode(' ', $_GET['terms']);
+            $postDataList = $this->postModel->getSearchedPosts($keywords);
+
+            foreach ($postDataList as &$postData) {
+                $postData['belongs_to_current_user'] = AuthService::isCurrentUser($postData['username']);
+                $postData = setLikeAndSaveStatus($postData, $isLoggedIn, $this->likeModel, $this->saveModel);
+            }
+            unset($postData);
+        }
+
+        // This view uses: $isLoggedIn, $isAdmin, $showResults, $postDataList
         require_once __DIR__."/../views/search-view.php";
     }
 }
