d090a65387ae0eb86bcdcbdcbe515610dcb94d72
iansteyn <iansteyn03@gmail.com>
2025-03-28 13:11:41 -0700

    add handleSearchInput() JS ajax function

--- code/public/scripts/admin.js
+++ code/public/scripts/admin.js
@@ -5,4 +5,26 @@
  * by sending AJAX requests to the server and receiving its responses.
  */
 
-console.log('hello');
+const searchInput = document.querySelector('.search-bar input[type="search"]');
+
+searchInput.addEventListener('input', ()=> handleSearchInput());
+
+// More AJAX!
+function handleSearchInput() {
+    let searchTerm = searchInput.value;
+
+    fetch(`/admin/search-users?terms=${encodeURIComponent(searchTerm)}`)
+    .then(response =>                   // Recieve response from server
+        response.json()
+    )
+    .then(data => {                     // perform action based on response
+        updateUserList(data.usernames)
+    })
+    .catch(error => {                   // catch and log network errors
+        console.error('Error:', error);
+    });
+}
+
+function updateUserList(usernames) {
+    console.log(usernames);
+}
