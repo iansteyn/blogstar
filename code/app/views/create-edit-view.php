477eca489daa10d840aff9bf174305c93955dbb9
iansteyn <iansteyn03@gmail.com>
2025-04-05 17:58:48 -0700

    Reformatting for post/discard buttons

--- code/app/views/create-edit-view.php
+++ code/app/views/create-edit-view.php
@@ -18,24 +18,32 @@
     <?php include __DIR__."/components/side-nav-component.php" ?>
   </header>
   <main>
-    <h1 class="page-title"><?= $isEditMode ? 'Edit your post' : 'Create your post' ?></h1>
+    <h1 class="page-title">
+      <?= $isEditMode ? 'Edit your post' : 'Create your post' ?>
+    </h1>
     <form class="panel create-panel" id="post-form" method="post" action="<?= $formAction ?>" enctype="multipart/form-data">
-      <label for="post-title">Title</label>
-      <textarea class="blog-title" id="post-title" name="post-title" placeholder="Write your title here!" required><?= $isEditMode ? htmlspecialchars($postData['post_title']) : '' ?></textarea>
-      <label for="post-text">Post</label>
+      <label for="post-title">
+        Title
+      </label>
+      <textarea class="blog-title" id="post-title" name="post-title" placeholder="Write your title here!" required>
+        <?= $isEditMode ? htmlspecialchars($postData['post_title']) : '' ?>
+      </textarea>
+      <label for="post-text">
+        Post
+      </label>
       <textarea class="blog-text" id="post-body" name="post-body" placeholder="Write your post here!" required><?= $isEditMode ? htmlspecialchars($postData['post_body']) : '' ?></textarea>
       <div class="form-group">
-        <label for="post-image"><?= $isEditMode ? 'Change photo (optional)' : 'Upload a photo' ?></label>
-        <input type="file" id="post-image" name="post-image" accept="image/png, image/jpeg, image/jpg, image/gif" <?= $imageRequired ?>/>
+        <label for="post-image">
+          <?= $isEditMode ? 'Change photo (optional)' : 'Upload a photo' ?>
+        </label>
+        <input type="file" id="post-image" name="post-image" accept="image/png, image/jpeg, image/jpg, image/gif" <?= $imageRequired ?>>
       </div>
-      <button class='post-button' id="submit-post-button" type="submit" value="Post"><?= $submitButtonText ?></button>
-      <?php if ($isEditMode): ?>
-        <a href="/blog-post/<?= $postData['post_id'] ?>" class="button-link">
-          <input class='post-button' id="cancel-edit-button" type="button" value="Cancel">
-        </a>
-      <?php else: ?>
-        <input class='post-button' id="discard-post-button" type="button" value="Discard">
-      <?php endif; ?>
+      <button class='post-button' id="submit-post-button" type="submit" value="Post">
+        <?= $submitButtonText ?>
+      </button>
+      <a href="/blog-post/<?= $postData['post_id'] ?>" class="button-link">
+        <input class='post-button' id="discard-button" type="button" value="<?= $isEditMode ? 'Cancel' : 'Discard' ?>">
+      </a>
     </form>
   </main>
 </body>
