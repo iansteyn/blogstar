5070505577a70d694dc019911c022c237e501966
iansteyn <iansteyn03@gmail.com>
2025-04-05 18:11:38 -0700

    Break up multi-attribute tags into multiple lines for readability

--- code/app/views/create-edit-view.php
+++ code/app/views/create-edit-view.php
@@ -3,7 +3,6 @@
     $isEditMode = isset($postData);
     $pageTitle = $isEditMode ? 'Edit Post' : 'Create Post';
     $formAction = $isEditMode ? "/post/edit/{$postData['post_id']}" : "/create";
-    $submitButtonText = $isEditMode ? 'Update' : 'Post';
     $imageRequired = !$isEditMode ? 'required' : '';
 
     echo generateDocumentHead(
@@ -21,29 +20,69 @@
     <h1 class="page-title">
       <?= $isEditMode ? 'Edit your post' : 'Create your post' ?>
     </h1>
-    <form class="panel create-panel" id="post-form" method="post" action="<?= $formAction ?>" enctype="multipart/form-data">
+    <form
+      class="panel create-panel"
+      id="post-form"
+      method="post"
+      action="<?= $formAction ?>"
+      enctype="multipart/form-data"
+    >
       <label for="post-title">
         Title
       </label>
-      <textarea class="blog-title" id="post-title" name="post-title" placeholder="Write your title here!" required>
+      <textarea
+        class="blog-title"
+        id="post-title"
+        name="post-title"
+        placeholder="Write your title here!"
+        required
+      >
         <?= $isEditMode ? htmlspecialchars($postData['post_title']) : '' ?>
       </textarea>
+
       <label for="post-body">
         Body
       </label>
-      <textarea class="blog-text" id="post-body" name="post-body" placeholder="Write your post here!" required><?= $isEditMode ? htmlspecialchars($postData['post_body']) : '' ?></textarea>
+      <textarea
+        class="blog-text"
+        id="post-body"
+        name="post-body"
+        placeholder="Write your post here!"
+        required
+      >
+        <?= $isEditMode ? htmlspecialchars($postData['post_body']) : '' ?>
+      </textarea>
+
       <div class="form-group">
         <label for="post-image">
           <?= $isEditMode ? 'Change photo (optional)' : 'Upload a photo' ?>
         </label>
-        <input type="file" id="post-image" name="post-image" accept="image/png, image/jpeg, image/jpg, image/gif" <?= $imageRequired ?>>
+        <input
+          type="file"
+          id="post-image"
+          name="post-image"
+          accept="image/png, image/jpeg, image/jpg, image/gif"
+          <?= $imageRequired ?>
+        >
       </div>
-      <button class='post-button' id="submit-post-button" type="submit" value="Post">
-        <?= $submitButtonText ?>
+
+      <button
+        class='post-button'
+        id="submit-post-button"
+        type="submit"
+        value="Post"
+      >
+        <?= $isEditMode ? 'Update' : 'Post' ?>
       </button>
       <a href="/blog-post/<?= $postData['post_id'] ?>" class="button-link">
-        <input class='post-button' id="discard-button" type="button" value="<?= $isEditMode ? 'Cancel' : 'Discard' ?>">
+        <input
+          class='post-button'
+          id="discard-button"
+          type="button"
+          value="<?= $isEditMode ? 'Cancel' : 'Discard' ?>"
+        >
       </a>
+
     </form>
   </main>
 </body>
