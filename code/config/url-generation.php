5629c58570761ad6fa12fed4ad43aa6819eb42d0
iansteyn <iansteyn03@gmail.com>
2025-04-06 14:08:51 -0700

    Move url-generation.php to config/ folder

---
+++ code/config/url-generation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * url-generation.php defines helper functions for generating URLs
+ * 
+ * It should be required once at the top of index.php,
+ * with all other scripts assuming its availablity.
+ * (since it is cumbersome to include it in literally every file).
+ */
+
+require_once __DIR__.'/EnvironmentConfig.php';
+
+
+
+
+//TODO: define routeUrl()
+//TODO: define resourceURL()
