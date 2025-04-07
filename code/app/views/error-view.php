8c3c69dadf0dc7c675f7f0e26c4aba19a47a5676
iansteyn <iansteyn03@gmail.com>
2025-04-07 16:37:36 -0700

    Update error view to use variables

--- code/app/views/error-view.php
+++ code/app/views/error-view.php
@@ -1,6 +1,13 @@
 <?php
-    require_once __DIR__."/../helpers/view-helpers.php";
-    echo generateDocumentHead('404 Not Found', [], []);
+/**
+ * This view expects the following variables:
+ * @var bool $isAdmin
+ * @var bool $isLoggedIn
+ * @var array $errorData with keys 'code', 'name', 'message'
+ */
+
+require_once __DIR__."/../helpers/view-helpers.php";
+echo generateDocumentHead('404 Not Found', [], []);
 ?>
 
 <body>
@@ -9,12 +16,12 @@
   </header>
   <main>
     <header class="page-header">
-        <h1>404</h1>
+        <h1><?= $errorData['code'] ?></h1>
     </header>
     <div class="panel">
       <section>
-        <h2>oops</h2>
-        <p>The page you were looking for does not exist.</p>
+        <h2><?= $errorData['name'] ?></h2>
+        <p><?= $errorData['message'] ?></p>
       </section>
   </main>
 </body>
