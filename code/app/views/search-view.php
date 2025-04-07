eab656109b33b6d35ee28ad1288823340f767391
iansteyn <iansteyn03@gmail.com>
2025-04-06 18:26:31 -0700

    bug-fix: missing (') in search-view

--- code/app/views/search-view.php
+++ code/app/views/search-view.php
@@ -47,7 +47,7 @@ echo generateDocumentHead(
             <?= empty($postDataList) ? ':( No' :  'Showing' ?>
             results for "<?= $searchValue ?>"
           </h3>
-          <a class='clear-link' href='<?= routeUrl('/search') ?>>
+          <a class='clear-link' href='<?= routeUrl('/search') ?>'>
             Clear
           </a>
         </div>
