f0860daf6dbb061f39d7ef0d2495f2791e92de10
iansteyn <iansteyn03@gmail.com>
2025-04-07 21:10:16 -0700

    Move boolean functions to new AuthStatus class, add more docs

--- code/app/services/AuthAccess.php
+++ code/app/services/AuthAccess.php
@@ -1,44 +1,29 @@
 <?php
 
-/* Note: yes these two services are a little too coupled
-   but its only because we have no access to server config stuff. */
 require_once __DIR__.'/ErrorService.php';
+require_once __DIR__.'/AuthStatus.php';
 
-class AuthService {
+/**
+ * A simple service class with static methods for authentication-based access control.
+ */
+class AuthAccess {
 
-  /**
-   * @param array $allowedRoles
-   * Checks if users are logged in, and redirects them to the login page if not.
-   * Checks user roles for restricted pages (e.g., admin page).
-   */
-  public static function requireAuth(array $allowedRoles) {
-    if ( ! AuthService::isLoggedIn()) {
-        header('location: '.routeUrl('/login'));
-        exit;
+    /**
+     * Call at the start of a controller function to restrict page/request access to certain roles.
+     * Redirects to login if user is not logged in.
+     * Redirects to forbidden error if user is logged but has incorrect role.
+     * @param array $allowedRoles array of strings
+     */
+    public static function requireAuth(array $allowedRoles) {
+
+        if (! AuthStatus::isLoggedIn()) {
+            header('location: ' . routeUrl('/login'));
+            exit;
+        }
+
+        if (! in_array($_SESSION['role'], $allowedRoles)) {
+            ErrorService::forbidden();
+        }
     }
-
-    if ( ! in_array($_SESSION['role'], $allowedRoles)) {
-        ErrorService::forbidden();
-    }
-  }
-
-  /**
-   * @return bool true if client is logged in, false otherwise
-   */
-  public static function isLoggedIn(): bool {
-    return (isset($_SESSION['username']) and ! empty($_SESSION['username']));
-  }
-
-  /**
-   * @return bool true if client is logged in and admin, false otherwise
-   */
-  public static function isAdmin(): bool {
-    return (AuthService::isLoggedIn() and $_SESSION['role'] == 'admin');
-  }
-
-  public static function isCurrentUser(?string $username): bool {
-    return (AuthService::isLoggedIn() and $_SESSION['username'] == $username);
-  } 
 }
-
 ?>
