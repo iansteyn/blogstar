548f7517e838fd0ae1b262ce49e5bbe79b55f0c7
iansteyn <iansteyn03@gmail.com>
2025-04-07 21:45:36 -0700

    add function Redirector::route()

--- code/app/routing/Redirector.php
+++ code/app/routing/Redirector.php
@@ -5,4 +5,13 @@
  */
 class Redirector {
 
+    /**
+     * Redirects the request to the given route,
+     * and terminates current script execution.
+     */
+    public static function route(string $path) {
+        header('location: '.routeUrl($path));
+        exit;
+    }
 }
+?>
