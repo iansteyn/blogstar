caca9e2122b6b2036b2567ed88b523e3042bb99b
iansteyn <iansteyn03@gmail.com>
2025-04-10 18:23:19 -0700

    add basic code for adding and removing error to file inputs with > 2 MB

--- code/public_html/scripts/image-size-limit.js
+++ code/public_html/scripts/image-size-limit.js
@@ -4,8 +4,40 @@ This is a last-minute addition
 It simply ensures that uploaded images cannot be more than 2 MB,
 since this is the max_allowed_packet for the remote server.
 */
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
 
-form = document.querySelector('form::has(input[type="file"])');
+const forms = Array.from(document.querySelectorAll('form'));
+const formWithFileInput = forms.filter(form => form.querySelector('input[type="file"]'))[0]; //there should only be one per page in the current site implementation
+const fileInput = formWithFileInput.querySelector('input[type="file"]');
 
-console.log(form);
+formWithFileInput.addEventListener ('submit', (submitEvent)=> {
+    if (fileInput.files[0].size > MAX_FILE_SIZE) {
+        submitEvent.preventDefault();
+        displayError(fileInput, "File must be less than 2MB.");
+    }
+});
+
+fileInput.addEventListener('input', (inputEvent)=> {
+    removeError(fileInput);
+});
+
+// HELPERS
+// (copied from form-validation.js)
+function displayError(input, message) {
+    const div = document.createElement("div");
+    div.textContent = message;
+    div.className = "error-message";
+    div.style.color = "var(--color-error)";
+    div.style.maxWidth = "42ch";
+    input.parentElement.appendChild(div);
+    input.classList.add("validation-error");
+}
+
+function removeError(input) {
+    const error = input.parentElement.querySelector(".error-message");
+    if (error) {
+        error.remove();
+        input.classList.remove("validation-error");
+    }
+}
 
